3291be2492d878dba8b38e121e5069e674070ec4
menynexi <meny_nexi@outlook.com>
2020-04-06 14:28:08 -0600

    filled out most out a template

--- java/src/NewSorting.java
+++ java/src/NewSorting.java
@@ -1,7 +1,31 @@
 class NewSorting{
 
     public static void newSorting(int[] A, int size){
+        //size tells you what you are sorting and can be a random number
+        if(A.length <= size){
+            toQuicksort(A);
+        }
+        else{
+            int mid = A.length/2;
+            int[] LH = new int[mid];//always mid size
+            int[] RH = new int[A.length - mid];
 
+            //populate LH,RH with loop populate with LH and RH
+
+            //recursivebly call
+            newSorting(LH,size);
+            newSorting(RH, size);
+
+            //merge them both write both methods
+            mergeSortedHalfes(A,LH,RH);
+        }
+    }
+    public static void mergeSortedHalfes(int[] A, int[] LH, int[] RH){
+        //where LH and RH is copied to A
+    }
+
+    public static void toQuicksort(int[] A){
+        //this is going to quick sort everything
     }
 
 }
