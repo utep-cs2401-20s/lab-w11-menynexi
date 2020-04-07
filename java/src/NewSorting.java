d9b48bbaa42b6ac54bb6d793bb18927ff3d650a6
menynexi <meny_nexi@outlook.com>
2020-04-07 11:09:00 -0600

    created populate method

--- java/src/NewSorting.java
+++ java/src/NewSorting.java
@@ -8,7 +8,10 @@ class NewSorting{
         else{
             int mid = A.length/2;
             int[] LH = new int[mid];//always mid size
+            populateArray(A, LH,0, mid);
             int[] RH = new int[A.length - mid];
+            populateArray(A,RH, mid + 1, A.length);
+
 
             //populate LH,RH with loop populate with LH and RH
 
@@ -22,10 +25,17 @@ class NewSorting{
     }
     public static void mergeSortedHalfes(int[] A, int[] LH, int[] RH){
         //where LH and RH is copied to A
+
     }
 
     public static void toQuicksort(int[] A){
         //this is going to quick sort everything
+    }
+
+    public static void populateArray(int[] A, int[] arr, int start,int  end){
+        for(int i = start; i < end; i++){
+            arr[i] = A[i];
+        }
     }
 
 }
--- java/src/NewSortingTester.java
+++ java/src/NewSortingTester.java
@@ -1,16 +1,20 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
 
-public class SortOfSortTester {
+public class NewSortingTester{
 
+    //This test case was created to test if the populateArray worked correctly im taking a bottom up approach
+
+    //this test case was created to test if the array can be populated
     @Test
     public void test1(){
         int[] arr = {7, 8, 6, 9, 3};
+        int[] LH = new int[arr.length];
 
-        NewSorting.newSorting();
-        assertArrayEquals();
+        NewSorting.populateArray(arr, LH, 0,arr.length);
+        assertArrayEquals(LH, arr);
     }
+
+
 
 }
