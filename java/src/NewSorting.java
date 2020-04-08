d9392f1de80ee4cc494ea44e02e87caa1f9b5ca0
menynexi <meny_nexi@outlook.com>
2020-04-07 20:46:16 -0600

    finished test cases and debugged code removed populate method

--- java/src/NewSorting.java
+++ java/src/NewSorting.java
@@ -1,41 +1,91 @@
 class NewSorting{
 
     public static void newSorting(int[] A, int size){
+        if(size <= 0){
+            size = 1;
+        }
         //size tells you what you are sorting and can be a random number
         if(A.length <= size){
-            toQuicksort(A);
+            toQuicksort(A, 0, A.length - 1);
         }
         else{
+
             int mid = A.length/2;
             int[] LH = new int[mid];//always mid size
-            populateArray(A, LH,0, mid);
-            int[] RH = new int[A.length - mid];
-            populateArray(A,RH, mid + 1, A.length);
+            int[] RH = new int[mid];
+
+            //try forcefully populating them
+            for(int i = 0; i < LH.length; i++) {
+                LH[i] = A[i];
+            }
+
+            for(int i = 0; i < RH.length; i++) {
+                RH[i] = A[mid++];
+            }
 
 
-            //populate LH,RH with loop populate with LH and RH
-
-            //recursivebly call
-            newSorting(LH,size);
+            newSorting(LH, size);
             newSorting(RH, size);
 
-            //merge them both write both methods
-            mergeSortedHalfes(A,LH,RH);
+            mergeSortedHalves(A,LH,RH);
         }
     }
-    public static void mergeSortedHalfes(int[] A, int[] LH, int[] RH){
-        //where LH and RH is copied to A
 
-    }
+    public static void mergeSortedHalves(int[] A, int[] LH, int[] RH){
+        int lh = 0;
+        int rh = 0;
 
-    public static void toQuicksort(int[] A){
-        //this is going to quick sort everything
-    }
-
-    public static void populateArray(int[] A, int[] arr, int start,int  end){
-        for(int i = start; i < end; i++){
-            arr[i] = A[i];
+        for(int i = 0; i < A.length; i++){
+            if(lh < LH.length && rh < RH.length){
+                if(LH[lh] <= RH[rh]){
+                    A[i] = LH[lh];
+                    lh++;
+                }
+                else{
+                    A[i] = RH[rh];
+                    rh++;
+                }
+            }
+            else if(lh < LH.length){
+                A[i] = LH[lh];
+                lh++;
+            }
+            else if (rh < RH.length){
+                A[i] = RH[rh];
+                rh++;
+            }
         }
+
+    }
+
+    public static void toQuicksort(int arr[], int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partitionOfQuicksort(arr, begin, end);
+
+            toQuicksort(arr, begin, partitionIndex-1);
+            toQuicksort(arr, partitionIndex+1, end);
+        }
+    }
+    
+    public static int partitionOfQuicksort(int arr[], int begin, int end) {
+        int pivot = arr[end];
+        int i = (begin-1);
+
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                int swapTemp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = swapTemp;
+            }
+        }
+
+        int swapTemp = arr[i+1];
+        arr[i+1] = arr[end];
+        arr[end] = swapTemp;
+
+        return i+1;
     }
 
 }
--- java/src/NewSortingTester.java
+++ java/src/NewSortingTester.java
@@ -5,15 +5,113 @@ public class NewSortingTester{
 
     //This test case was created to test if the populateArray worked correctly im taking a bottom up approach
 
-    //this test case was created to test if the array can be populated
+    //this is used to test if mergeSortedHalves works when all the values are lower in LH first
     @Test
-    public void test1(){
-        int[] arr = {7, 8, 6, 9, 3};
-        int[] LH = new int[arr.length];
+    public void T1mergeSortedHalves(){
+        int[] arr = {1,3,4,5};
+        int[] LH = {1,3};
+        int[] RH = {4,5};
+        int[] exp = {1, 3, 4, 5};
 
-        NewSorting.populateArray(arr, LH, 0,arr.length);
-        assertArrayEquals(LH, arr);
+        NewSorting.mergeSortedHalves(arr, LH, RH);
+        assertArrayEquals(exp, arr);
     }
+
+    //this is used to test if mergeSortedHalves works when all the values are lower in RH first
+    @Test
+    public void T2mergeSortedHalves(){
+        int[] arr = {5,4,1,3};
+        int[] LH = {4,5};
+        int[] RH = {1,3};
+        int[] exp = {1, 3, 4, 5};
+
+        NewSorting.mergeSortedHalves(arr, LH, RH);
+        assertArrayEquals(exp, arr);
+    }
+
+    //this test was used when alternating values need to be implemented
+    @Test
+    public void T3mergeSortedHalves(){
+        int[] arr = {1,3,2,4};
+        int[] LH = {1,3};
+        int[] RH = {2,4};
+        int[] exp = {1, 2, 3, 4};
+
+        NewSorting.mergeSortedHalves(arr, LH, RH);
+        assertArrayEquals(exp, arr);
+    }
+
+    //the following test cases are to test quickstoer method
+    @Test
+    public void T4quicksort(){
+        int[] arr = {1, 3, 2, 4};
+        int[] exp = {1, 2, 3, 4};
+
+        NewSorting.toQuicksort(arr, 0, arr.length - 1);//arr.length -1 because it will
+        assertArrayEquals(exp, arr);
+    }
+
+    @Test// this is testing when the array is smaller than the size given it should only make a single call to quicksort
+    public void T5newSorting(){
+        int[] arr = {1, 3, 2, 4};
+        int[] exp = {1, 2, 3, 4};
+
+        NewSorting.newSorting(arr, 5);
+        assertArrayEquals(exp, arr);
+    }
+
+    @Test // I used this test case because when debugging my code I found out that RH was not populating correctly
+    // and it would populate RH with zeros this test case was used to test how RH was being populating
+    public void T6newSorting(){
+        int[] arr = {1, 3, 0, 0};
+        int[] exp = {0, 0, 1, 3};
+
+        NewSorting.newSorting(arr, 4);
+        assertArrayEquals(exp, arr);
+    }
+
+    @Test//this is testing when the array is bigger than the size given it should populate RH and LH correctly
+         // then make recursive calls that break down RH and LH till the size allows them to do a quick sort and merge them together
+    public void T7newSorting(){
+        int[] arr = {1, 3, 2, 4, 7};
+        int[] exp = {1, 2, 3, 4, 7};
+
+        NewSorting.newSorting(arr, 3);
+        assertArrayEquals(exp, arr);
+    }
+
+    @Test//this was used to test a larger number set and check if it still have problems with populateing RH correctly
+    public void T8newSorting(){
+        int[] arr = {3, 8, 9, 7, 5, 4, 13};
+        int[] exp = {3, 4, 5, 7, 8, 9, 13};
+
+        NewSorting.newSorting(arr, 3);
+        assertArrayEquals(exp, arr);
+    }
+
+    @Test// while testing size -1 I found out that it creates a stack overflow and went back and fixed it
+    public void T9newSorting(){
+        int[] arr = {12, 7, 5, 4, 22};
+        int[] exp = {4, 5, 7, 12, 22};
+
+        NewSorting.newSorting(arr, -1);
+        assertArrayEquals(exp, arr);
+    }
+
+    @Test
+    public void T10newSorting(){
+        int[] arr = {12, -7, 5, -4, 22};
+        int[] exp = {-7, -4, 5, 12, 22};
+
+        NewSorting.newSorting(arr, 3);
+        assertArrayEquals(exp, arr);
+    }
+
+
+
+
+
+
 
 
 
